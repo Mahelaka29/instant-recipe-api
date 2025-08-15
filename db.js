8bcfe78d72da927f7a3588b70bbc3eb1b3d44f1f
Mahelaka Mansoori <mahelakamansoori29@gmail.com>
2025-08-15 14:13:37 +0530

    ready for deployment

--- db.js
+++ db.js
@@ -3,12 +3,11 @@ import env from "dotenv";
 
 env.config();
 
-//database connection
 const db = new pg.Client({
   user: process.env.PG_USER,
   host: process.env.PG_HOST,
-  password: process.env.PG_PASSWORD,
   database: process.env.PG_DATABASE,
+  password: process.env.PG_PASSWORD,
   port: process.env.PG_PORT,
 });
 
