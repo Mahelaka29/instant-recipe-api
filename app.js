f1df5cba468336d2efb16a18606f65e5ce619f23
Mahelaka Mansoori <mahelakamansoori29@gmail.com>
2025-08-15 13:46:04 +0530

    ready for deployment

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ import db from "./db.js";
 
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 env.config();
 
 const __filename = fileURLToPath(import.meta.url);
